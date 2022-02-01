7b2a5374d4c0551466a1e61a912e3c2274f4e78a
Rafhael <rafhael.dev@gmail.com>
2022-02-01 08:47:18 -0300

    add validation transitions

--- src/main/java/com/studies/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/com/studies/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -4,6 +4,7 @@ import com.studies.beer.order.service.domain.BeerOrderEventEnum;
 import com.studies.beer.order.service.domain.BeerOrderStatusEnum;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
@@ -20,6 +21,21 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
                 .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
                 .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+        transitions.withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+                .event(BeerOrderEventEnum.VALIDATE_ORDER)
+                //todo add validation action here
+                .and().withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+                .event(BeerOrderEventEnum.VALIDATION_PASSED)
+                .and().withExternal()
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .event(BeerOrderEventEnum.VALIDATION_FAILED);
 
     }
 }
