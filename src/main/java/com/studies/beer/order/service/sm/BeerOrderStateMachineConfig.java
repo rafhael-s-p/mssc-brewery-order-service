554577842caed0962cb3215fb8617901eb6719e2
Rafhael <rafhael.dev@gmail.com>
2022-02-04 09:01:17 -0300

    send validate order request with action

--- src/main/java/com/studies/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/com/studies/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -2,13 +2,22 @@ package com.studies.beer.order.service.sm;
 
 import com.studies.beer.order.service.domain.BeerOrderEventEnum;
 import com.studies.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
+@RequiredArgsConstructor
+@Configuration
+@EnableStateMachineFactory
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validateOrderAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -29,7 +38,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
         transitions.withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
-                //todo add validation action here
+                .action(validateOrderAction)
                 .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
                 .event(BeerOrderEventEnum.VALIDATION_PASSED)
---
+++ src/main/java/com/studies/brewery/model/events/ValidateOrderRequest.java
@@ -0,0 +1,17 @@
+package com.studies.brewery.model.events;
+
+import com.studies.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderRequest {
+
+    private BeerOrderDto beerOrder;
+
+}
