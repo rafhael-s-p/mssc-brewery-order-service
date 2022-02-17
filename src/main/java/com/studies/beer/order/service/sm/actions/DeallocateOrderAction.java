3793058cfd7116117b907a61290fedfc00d5793e
Rafhael <rafhael.dev@gmail.com>
2022-02-17 08:14:34 -0300

    adding deallocate order action

---
+++ src/main/java/com/studies/beer/order/service/sm/actions/DeallocateOrderAction.java
@@ -0,0 +1,43 @@
+package com.studies.beer.order.service.sm.actions;
+
+import com.studies.beer.order.service.config.JmsConfig;
+import com.studies.beer.order.service.domain.BeerOrder;
+import com.studies.beer.order.service.domain.BeerOrderEventEnum;
+import com.studies.beer.order.service.domain.BeerOrderStatusEnum;
+import com.studies.beer.order.service.repositories.BeerOrderRepository;
+import com.studies.beer.order.service.services.BeerOrderManagerImpl;
+import com.studies.beer.order.service.web.mappers.BeerOrderMapper;
+import com.studies.brewery.model.events.DeallocateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class DeallocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final JmsTemplate jmsTemplate;
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(UUID.fromString(beerOrderId));
+
+        beerOrderOptional.ifPresentOrElse(beerOrder -> {
+            jmsTemplate.convertAndSend(JmsConfig.DEALLOCATE_ORDER_QUEUE,
+                    DeallocateOrderRequest.builder()
+                            .beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder))
+                            .build());
+            log.debug("Sent Deallocation Request for order id: " + beerOrderId);
+        }, () -> log.error("Beer Order Not Found!"));
+    }
+}
